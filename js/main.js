8298500e39eb8228b74a1252372be4dc8d312235
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-04 14:52:39 +0100

    stampa i 5 numeri casuali in html, timeout hide numeri, prompt

--- js/main.js
+++ js/main.js
@@ -1,16 +1,43 @@
 'strict mode'
 
+const contain = document.querySelector(".container");
+let userNumber;
+let userNumberArray = [5
+];
+
 // Mi creo l'array di 5 numeri casuali e univoci
 let ranNamArr = [];
 let i = 0;
 while ( ranNamArr.length < 5 ){
     i++;
     let random = (Math.floor(Math.random() * 100) + 1);
-    // ranNamArr.push(random);
     if (ranNamArr.includes(random) === false ){
         ranNamArr.push(random);
-        // console.log(randomNumber);
     }
 }
+console.log("L'array di numeri casuali è " + ranNamArr);
 
-console.log(ranNamArr);
+//stampo i 5 numeri casuali in pagina
+
+for ( let i = 0; i < ranNamArr.length; i++){
+    let contNumber = document.createElement("div");
+    contain.append(contNumber);
+    contNumber.innerHTML = ranNamArr[i];
+}
+
+//il timeout che fa scomparire i numeri
+setTimeout(function(){
+    contain.classList.add("hide");
+    document.querySelector("h1").innerHTML = "???????????";
+}, 3000);
+
+// qui chiedo all'utente di scrivere i numeri che ricorda
+setTimeout(function(){
+    for (i = 0; i < 5; i++){
+        userNumber = Number(prompt("Inserisci il numero"));
+        userNumberArray.push(userNumber);
+    }
+    console.log("Questi sono i numeri inseriti dall'utente " + userNumberArray);
+
+}, 4000);
+
