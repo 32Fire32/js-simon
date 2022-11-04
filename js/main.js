64941192b044efa5ef26c07b2b2962f4ba257341
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-04 15:27:35 +0100

    inserisce le condizioni di fine gioco. Fine esercizio base

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 
 const contain = document.querySelector(".container");
 let userNumber;
-let userNumberArray = [5
-];
-
-// Mi creo l'array di 5 numeri casuali e univoci
+let userNumberArray = [];
 let ranNamArr = [];
 let i = 0;
+let gotNumbers = [];
+
+// Mi creo l'array di 5 numeri casuali e univoci
 while ( ranNamArr.length < 5 ){
     i++;
     let random = (Math.floor(Math.random() * 100) + 1);
@@ -39,5 +39,24 @@ setTimeout(function(){
     }
     console.log("Questi sono i numeri inseriti dall'utente " + userNumberArray);
 
+    // condizioni di vittoria o sconfitta
+for ( let i = 0; i < ranNamArr.length; i++){
+    if (userNumberArray.includes(ranNamArr[i])){
+        console.log(ranNamArr[i]);
+        gotNumbers.push(ranNamArr[i]);
+    }  
+}
+    alert(`i numeri ricordati sono ${gotNumbers.length} e sono ${gotNumbers}`);
+    console.log("I numeri ricordati sono " + gotNumbers); 
+    
+    if ( gotNumbers.length === ranNamArr.length) {
+        alert("complimenti hai ricordato tutti i numeri!")
+        contain.classList.remove("hide");
+    }
+
 }, 4000);
+
+
+
+
 
